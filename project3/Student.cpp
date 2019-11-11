baaaafc9e4470ca0af10e023da00ff0fa5ca605e
Justin Ventura <justinv9001@aol.com>
2019-11-11 18:22:35 -0500

    group project

---
+++ project3/Student.cpp
@@ -0,0 +1,45 @@
+#include "Student.h"
+#include <iostream>
+#include <iomanip>
+
+// Implementation of Copy Constructor
+Student::Student(const Student& obj)
+{
+	std::cout << "Student Constructed (COPY).\n";
+	this->Name = obj.Name;
+	this->ID = obj.ID;
+	this->Year = obj.Year;
+	this->GPA = obj.GPA;
+}
+
+// Implementation of Overloaded operator=
+void Student::operator=(const Student& obj)
+{
+	std::cout << "Student Constructed (AO).\n";
+	this->Name = obj.Name;
+	this->ID = obj.ID;
+	this->Year = obj.Year;
+	this->GPA = obj.GPA;
+}
+
+// Implementation of Overloaded operator<< (friend)
+std::ostream& operator<<(std::ostream& ostr, const Student& obj)
+{
+	ostr << std::left << std::setw(25) << obj.Name << "| ";
+	ostr << std::left << std::setw(9) << obj.ID << "| ";
+	ostr << " " << std::left << std::setw(3) << obj.Year << "| ";
+	ostr << std::left << std::setw(3) << std::setprecision(2) << std::fixed << obj.GPA;
+	return ostr;
+}
+
+// Implementation of Overloaded operator>> (friend)
+std::istream& operator>> (std::istream& istr, Student& obj)
+{
+	return istr;
+}
+
+// Implementation of Overloaded operator== (friend)
+bool operator==(const Student& lhs, const Student& rhs)
+{
+	return true;
+}
---
+++ project3/Student.h
@@ -0,0 +1,71 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+
+class Student
+{
+private:
+	std::string Name;
+	int ID;
+	int Year;
+	float GPA;
+
+public:
+	// -- STUDENT CLASS UTILITIES --
+
+	// Constructor
+	Student(std::string n = "N/A", int id = 0, int y = 0, float gpa = 0.0):
+			Name(n), ID(id), Year(y), GPA(gpa) 
+			{ std::cout << "Student Constructed.\n"; }
+
+	// Destructor
+	~Student() { std::cout << "Student destroyed.\n"; }
+	
+	// Copy Constructor
+	Student(const Student& obj);
+	
+	// Overloaded Assignment Operator
+	void operator=(const Student& obj);
+
+	// -- METHODS FOR STUDENT CLASS --
+
+	// Name getter
+	std::string getName()
+	{ return this->Name; }
+
+	// ID getter
+	int getID()
+	{ return this->ID; }
+
+	// Year getter
+	int getYear()
+	{ return this->Year; }
+
+	// GPA getter
+	float getGPA()
+	{ return this->GPA; }
+
+	// Name setter
+	void setName(std::string n)
+	{ this->Name = n; }
+
+	// ID setter
+	void setID(int id)
+	{ this->ID = id; }
+
+	// Year setter
+	void setYear(int y)
+	{ this->Year = y; }
+
+	// GPA setter
+	void setGPA(float gpa)
+	{ this->GPA = gpa; }
+
+	// Overloaded << operator
+	friend std::ostream& operator<<(std::ostream& ostr, const Student& obj);
+
+	// Overloaded >> operator
+	friend std::istream& operator>> (std::istream& istr, Student& obj);
+};
+
+#endif
